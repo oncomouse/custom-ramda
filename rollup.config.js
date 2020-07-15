36c9aecbf3d40a14ecf0ea8a9ac5529a290495ec
oncomouse <oncomouse@gmail.com>
2020-07-15 11:08:43 -0500

    Initial Commit

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var babel = require('rollup-plugin-babel');
+// uglify handles only es5 code, so this also acts as smoke test against shipping es2015+ syntax
+var uglify = require('rollup-plugin-uglify').uglify;
+var pkg = require('./package.json');
+
+var banner = '//  Ramda v' + pkg.version + '\n'
+  + '//  https://github.com/ramda/ramda\n'
+  + '//  (c) 2013-' + new Date().getFullYear() + ' Scott Sauyet, Michael Hurley, and David Chambers\n'
+  + '//  Ramda may be freely distributed under the MIT license.\n';
+
+var input = 'source/index.js';
+
+var config = {
+  input: input,
+  output: {
+    format: 'umd',
+    name: 'R',
+    exports: 'named',
+    banner: banner
+  },
+  plugins: [
+    babel({ presets: [['@babel/preset-env', { targets: { ie: '11' } }]]})
+  ]
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+      },
+      warnings: false
+    })
+  );
+}
+
+module.exports = config;
